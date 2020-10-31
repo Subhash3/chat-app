dfc963cd87b48875c744c821d911316ac30aef53
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-10-31 14:48:25 +0530

    Added emotes

--- client/src/components/NewMsgForm/NewMsgForm.jsx
+++ client/src/components/NewMsgForm/NewMsgForm.jsx
@@ -3,11 +3,14 @@ import { useCurrentUser } from '../../contexts/CurrentUserProvider'
 import { useActiveConversation } from '../../contexts/ActiveConversationProvider'
 import { useChatDB } from '../../contexts/ChatDBProvider'
 import { useSocket } from '../../contexts/SocketProvider'
+import Picker from 'emoji-picker-react';
+import SendIcon from '@material-ui/icons/Send';
 import { v4 as uuid } from 'uuid'
 import './NewMsgForm.min.css'
 
 const NewMsgForm = () => {
     const [newMsg, setNewMsg] = useState("")
+    const [openEmojiPicker, setOpenEmojiPicker] = useState(false)
     const [chatDB, setChatDB] = useChatDB()
     const [currentUser] = useCurrentUser()
     const [activeConversationID] = useActiveConversation()
@@ -26,7 +29,15 @@ const NewMsgForm = () => {
         }
     }, [activeConversationID, inputRef])
 
+    const onEmojiClick = (event, emojiObject) => {
+        setNewMsg(newMsg + emojiObject.emoji);
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+    };
+
     const sendMessage = (msgBody) => {
+        setOpenEmojiPicker(false)
         let msgObject = {
             id: uuid(),
             senderID: currentUser.id,
@@ -62,7 +73,18 @@ const NewMsgForm = () => {
                 onChange={handleChange}
                 ref={inputRef}
             />
-            <button type="submit" className="send-msg">Send</button>
+            <div
+                className="emotes"
+                onClick={(e) => { setOpenEmojiPicker(!openEmojiPicker) }}
+            >
+                😊
+            </div>
+            {
+                openEmojiPicker &&
+                <Picker preload={false} onEmojiClick={onEmojiClick} />
+            }
+
+            <button type="submit" className="send-msg"><SendIcon /></button>
         </form>
     );
 }
