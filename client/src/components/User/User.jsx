6cf9492c456648cf0a6f38bb240ea893e1ef853b
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-11-03 15:19:23 +0530

    Now fetch users from the backend

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -5,11 +5,11 @@ const User = ({ user, handleClick, active, online }) => {
     // console.log(user, active)
 
     let className = `user ${active ? "active" : ""} ${online ? "online" : ""}`
-
+    let username = user.name ? user.name : user.id
     return (
         <div className={className} onClick={handleClick} data-user-id={user.id}>
             <div className="image" data-user-id={user.id}></div>
-            <div className="name" data-user-id={user.id}>{user.name}</div>
+            <div className="name" data-user-id={user.id}>{username}</div>
         </div>
     )
 }
