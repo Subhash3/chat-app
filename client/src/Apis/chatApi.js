15bc07530d338e89ed93b58a71e2e27678ff795c
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-12-30 11:04:20 +0530

    Deploying latest version

--- client/src/Apis/chatApi.js
+++ client/src/Apis/chatApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const URL = "http://localhost:3000/chat-app"
-// const URL = "https://yml-chat-app.herokuapp.com/chat-app"
+// const URL = "http://localhost:3000/chat-app"
+const URL = "https://yml-chat-app.herokuapp.com/chat-app"
 
 export const chatAPI = axios.create({
     baseURL: URL
